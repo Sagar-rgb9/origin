861166a867fb1450f056bc4a6be56efcfb3f088f
$agar <atoz107010@gmail.com>
2022-07-13 12:59:14 +0530

    Updates

---
+++ precache-manifest.1d265460c3e8bdd058e9d966e0f68f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "127899641139b6ba5787",
+    "url": "/origin/static/css/main.2c0be1ff.chunk.css"
+  },
+  {
+    "revision": "127899641139b6ba5787",
+    "url": "/origin/static/js/main.bc65bd1a.chunk.js"
+  },
+  {
+    "revision": "5ce71b9de2cde7327302",
+    "url": "/origin/static/js/runtime~main.cfa93a9f.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/origin/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/origin/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "204ee9023e1e342131a2b066bda73933",
+    "url": "/origin/index.html"
+  }
+];
